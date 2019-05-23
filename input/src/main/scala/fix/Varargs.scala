be27eca1396168c8a4ebbeb0a79b213c53deb84a
Lukas Rytz <lukas.rytz@gmail.com>
2019-05-23 15:40:03 +0200

    moar tests for varargs

--- input/src/main/scala/fix/Varargs.scala
+++ input/src/main/scala/fix/Varargs.scala
@@ -5,4 +5,6 @@ package fix
 
 class Varargs {
   def foo(xs: scala.collection.Seq[String]) = List(xs: _*)
+  def bar = List(List(1,2,3): _*)
+  def baz = List(scala.Seq(1,2,3): _*)
 }
--- output/src/main/scala/fix/Varargs.scala
+++ output/src/main/scala/fix/Varargs.scala
@@ -2,4 +2,6 @@ package fix
 
 class Varargs {
   def foo(xs: scala.collection.Seq[String]) = List(xs.toSeq: _*)
+  def bar = List(List(1,2,3): _*)
+  def baz = List(scala.Seq(1,2,3): _*)
 }
