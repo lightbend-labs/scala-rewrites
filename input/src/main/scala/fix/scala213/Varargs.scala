0adc2e1659e68fb0c8a0de44b68c44b393c82d14
Lukas Rytz <lukas.rytz@gmail.com>
2019-05-23 16:08:24 +0200

    add todo for varargs

--- input/src/main/scala/fix/scala213/Varargs.scala
+++ input/src/main/scala/fix/scala213/Varargs.scala
@@ -7,4 +7,6 @@ class Varargs {
   def foo(xs: scala.collection.Seq[String]) = List(xs: _*)
   def bar = List(List(1,2,3): _*)
   def baz = List(scala.Seq(1,2,3): _*)
+  // TODO
+  // def biz = List(scala.collection.mutable.ArrayBuffer(1,2): _*)
 }
