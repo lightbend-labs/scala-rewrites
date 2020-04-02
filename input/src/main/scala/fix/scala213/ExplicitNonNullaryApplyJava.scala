ecee4166f641d3cef1d3a410df709afee38c6f90
Dale Wijnand <dale.wijnand@gmail.com>
2020-04-02 22:15:49 +0100

    ExplicitNonNullaryApply sad story for Java methods
    What we'd like is to leave any call to a method that was defined in Java
    (or any "universal" method, like ##, eq, isInstanceOf) to be exempt.
    
    It appears Scalameta/Semanticdb/Scalafix doesn't capture that
    information, and thus basically all of them get parens... :(
    
    Apparently in Metals they use the presentation compiler to recover all
    the missing info and I know in ScalaClean they have their own nsc
    Traverser, I'm guessing for the same reason.

---
+++ input/src/main/scala/fix/scala213/ExplicitNonNullaryApplyJava.scala
@@ -0,0 +1,70 @@
+/*
+rule = fix.scala213.ExplicitNonNullaryApply
+*/
+package fix.scala213
+
+import ExplicitNonNullaryApplyJavaDefs._
+
+class ExplicitNonNullaryApplyJava {
+  any.##
+  any.getClass()
+  any.getClass
+  any.hashCode()
+  any.hashCode
+  any.toString()
+  any.toString
+
+  ref.##
+  ref.getClass()
+  ref.getClass
+  ref.hashCode()
+  ref.hashCode
+  ref.toString()
+  ref.toString
+
+  obj.##
+  obj.getClass()
+  obj.getClass
+  obj.hashCode()
+  obj.hashCode
+  obj.toString()
+  obj.toString
+
+  str.##
+  str.getClass()
+  str.getClass
+  str.hashCode()
+  str.hashCode
+  str.toString()
+  str.toString
+
+  cm.toString()
+  cm.toString
+  cp.toString()
+  cp.toString
+
+  cm.run()
+  cm.run
+  cp.run()
+  cp.run
+
+  cm.foo()
+  cm.foo
+  cp.foo()
+  cp.foo
+
+  vcm.toString()
+  vcm.toString
+  vcp.toString()
+  vcp.toString
+
+  ccm.toString()
+  ccm.toString
+  ccp.toString()
+  ccp.toString
+
+  vccm.toString()
+  vccm.toString
+  vccp.toString()
+  vccp.toString
+}
