386de1ba904636eafa0e943cdf37a07da5c12b9b
kenji yoshida <6b656e6a69@gmail.com>
2020-07-20 13:25:58 +0900

    restore ScalaNightlyPlugin change
    https://github.com/scala/community-build/commit/11ab59aacd460013acc802f1204612bee0224e2e

--- project/ScalaNightlyPlugin.scala
+++ project/ScalaNightlyPlugin.scala
@@ -22,8 +22,7 @@ object ScalaNightlyPlugin extends AutoPlugin {
 
   override def projectSettings = ifNightly(
     scalaVersion := ((LocalRootProject / scalaVersion).value match {
-    //case BinCompatV(x, y, z, _, _) => s"$x.$y.${z.toInt - 1}" // Restore when the 2.12 CB is bumped
-      case BinCompatV(x, y, z, _, _) => s"$x.$y.$z"
+      case BinCompatV(x, y, z, _, _) => s"$x.$y.${z.toInt - 1}"
       case sv                        => sv
     }),
     scalaInstance := {
