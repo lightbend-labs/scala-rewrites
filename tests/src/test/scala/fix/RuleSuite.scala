c4b330d4bc05a3c0a665e5e6c3aa9e3b21937266
unarist <m.unarist@gmail.com>
2023-08-18 04:00:28 +0900

    Bump scalafix to 0.11.0
    This update also bumps scalatest to 3.2.x, which includes renaming traits.
    
    see also: https://www.scalatest.org/release_notes/3.2.0

--- tests/src/test/scala/fix/RuleSuite.scala
+++ tests/src/test/scala/fix/RuleSuite.scala
@@ -1,10 +1,11 @@
 package fix
 
 import scala.reflect.ensureAccessible
-import org.scalatest.{ ConfigMap, FunSpecLike }
+import org.scalatest.ConfigMap
+import org.scalatest.funspec.AnyFunSpecLike
 import scalafix.testkit.AbstractSemanticRuleSuite
 
-class RuleSuite extends AbstractSemanticRuleSuite with FunSpecLike with BeforeAndAfterAllConfigMapAlt {
+class RuleSuite extends AbstractSemanticRuleSuite with AnyFunSpecLike with BeforeAndAfterAllConfigMapAlt {
   val isSaveExpectField = ensureAccessible(classOf[AbstractSemanticRuleSuite].getDeclaredField("isSaveExpect"))
 
   // If you invoke the test as "tests/testOnly -- -Doverwrite=true" it will save the expected
